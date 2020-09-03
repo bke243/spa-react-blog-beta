813475607a0fd636ebf038ee4ce734abdac28b88
peter <peterbadibake@gmail.com>
2020-09-03 02:01:58 +0200

    Updates

---
+++ precache-manifest.19113efd18d8ba54bff19d6f9f3f3bc0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0a98919dead633b90fd646309d98ab",
+    "url": "/spa-react-blog-beta/index.html"
+  },
+  {
+    "revision": "b8c51fed25ff1479fd1b",
+    "url": "/spa-react-blog-beta/static/css/main.83ff1190.chunk.css"
+  },
+  {
+    "revision": "5eaf1f2b0a20cc7c69ca",
+    "url": "/spa-react-blog-beta/static/js/2.2ea0d433.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spa-react-blog-beta/static/js/2.2ea0d433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c51fed25ff1479fd1b",
+    "url": "/spa-react-blog-beta/static/js/main.8ece20b5.chunk.js"
+  },
+  {
+    "revision": "feebae15a5a9126b8250",
+    "url": "/spa-react-blog-beta/static/js/runtime-main.e0e9d787.js"
+  },
+  {
+    "revision": "f6913ac5ea548c765a2fd0fff8f885fb",
+    "url": "/spa-react-blog-beta/static/media/AboutMeIcon.f6913ac5.svg"
+  },
+  {
+    "revision": "6b8cbe4735da0828fa05cb0c49818d3f",
+    "url": "/spa-react-blog-beta/static/media/aboutButton.6b8cbe47.svg"
+  },
+  {
+    "revision": "b63250d738e4411cdf7c92761ecfa132",
+    "url": "/spa-react-blog-beta/static/media/apiIcon.b63250d7.svg"
+  },
+  {
+    "revision": "1986610b1d6b4e2f2c2d5b6adc229761",
+    "url": "/spa-react-blog-beta/static/media/backendIcon.1986610b.svg"
+  },
+  {
+    "revision": "429555d108bdea2f06b3f894861c33f4",
+    "url": "/spa-react-blog-beta/static/media/contactIcon.429555d1.svg"
+  },
+  {
+    "revision": "6e0189df1aa560ae4749aa42513b75fc",
+    "url": "/spa-react-blog-beta/static/media/djena.6e0189df.jfif"
+  },
+  {
+    "revision": "8b132209365abd5364619137937599a4",
+    "url": "/spa-react-blog-beta/static/media/emailIcon.8b132209.svg"
+  },
+  {
+    "revision": "ed304557221cbd8c2dda5aff66bd4e06",
+    "url": "/spa-react-blog-beta/static/media/footer5-01.ed304557.svg"
+  },
+  {
+    "revision": "06cf7ead0954bd4f34ee4c55c97384a1",
+    "url": "/spa-react-blog-beta/static/media/fullstackIcon.06cf7ead.svg"
+  },
+  {
+    "revision": "e1a7c872b37b6b697aebc2dc848d2115",
+    "url": "/spa-react-blog-beta/static/media/heydar.e1a7c872.jfif"
+  },
+  {
+    "revision": "19ca196a7208956a192873aad990d0d6",
+    "url": "/spa-react-blog-beta/static/media/house.19ca196a.svg"
+  },
+  {
+    "revision": "bc7203098db95f5ef8d9e438c9d048ff",
+    "url": "/spa-react-blog-beta/static/media/kuba.bc720309.jpg"
+  },
+  {
+    "revision": "14e54edd3aa3f587bd31834b96b6da89",
+    "url": "/spa-react-blog-beta/static/media/laptopbackground.14e54edd.jpg"
+  },
+  {
+    "revision": "dca58b339c55046e37de83e32836cb36",
+    "url": "/spa-react-blog-beta/static/media/location.dca58b33.svg"
+  },
+  {
+    "revision": "296a8cbbfb5cff9939d83aa782212f8c",
+    "url": "/spa-react-blog-beta/static/media/peter.296a8cbb.jpg"
+  },
+  {
+    "revision": "f761e4994ba7601d23cda0afc1f726ca",
+    "url": "/spa-react-blog-beta/static/media/peter.f761e499.jfif"
+  },
+  {
+    "revision": "145121068bfd87bde5ee14e9e9d7968d",
+    "url": "/spa-react-blog-beta/static/media/phoneIcon.14512106.svg"
+  },
+  {
+    "revision": "1f10604d739fdeecfb45284bd0bbd02a",
+    "url": "/spa-react-blog-beta/static/media/portfolioIcon.1f10604d.svg"
+  },
+  {
+    "revision": "35abfcfb7467e9f91fdb972ac7a899ca",
+    "url": "/spa-react-blog-beta/static/media/reactIcon.35abfcfb.svg"
+  },
+  {
+    "revision": "61e9e973e44f6a5700f75a26c84f4935",
+    "url": "/spa-react-blog-beta/static/media/servicesIcon.61e9e973.svg"
+  },
+  {
+    "revision": "5de4997ef132ee1d451393550e049225",
+    "url": "/spa-react-blog-beta/static/media/testimonialsIcon.5de4997e.svg"
+  },
+  {
+    "revision": "8564c75b131c07a70099031977b2350f",
+    "url": "/spa-react-blog-beta/static/media/toolbarBackground.8564c75b.jpg"
+  }
+]);
